83a8a52000efe1f9f757a6f2fba5941753523f3e
Ben <bburrva@gmail.com>
2017-01-21 11:07:38 -0500

    added spring initilizr files. included web, devtools, H2, and JPA

---
+++ src/main/java/com/theironard/MastermindBackendApplication.java
@@ -0,0 +1,12 @@
+package com.theironard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MastermindBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MastermindBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/com/theironard/MastermindBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.theironard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MastermindBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
